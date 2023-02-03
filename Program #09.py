bfda6854ffb6e9e806df5f36c0c7734fbbedb464
Muhammad-Rebaal <122050424+Muhammad-Rebaal@users.noreply.github.com>
2023-02-03 13:42:23 -0800

    Add files via upload

---
+++ Program #09.py
@@ -0,0 +1,19 @@
+                                #Swap Two Numbers
+
+a = 10
+b = 12
+
+# print("")
+# print(f"The value of a is {a} and the value of b is {b} .")
+
+# print("")
+
+# d=a
+# a=b
+# b=d
+
+# print(f"The value of a is {a} and the value of b is {b} .")
+# print("")
+
+a,b=b,a
+print(f"The value of a is {a} and the value of b is {b} .")
